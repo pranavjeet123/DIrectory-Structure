2efa5604997c6b0869cfb96be592b7ebf7b2cf01
pranav <pranavjeetmishra@gmail.com>
2020-04-23 12:19:35 +0530

    Added hardcoded html and css to check alignment

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,22 @@ let directory =["file1","file2",{
         gallery:["image1","image2"]
     }]
 }];
+
+
+let cbFn= function(){
+    console.log("clicked");
+    }
+
+
+let yellowdiv = document.querySelector('[data-yellow]');
+yellowdiv.addEventListener("click",cbFn);
+
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
